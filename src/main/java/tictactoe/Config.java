2689f75128d8628cbcb40f1428c48666448f6bd9
Chris Cowell-Shah <chris.cowell-shah@jivesoftware.com>
2014-05-18 15:05:23 -0700

    add training vs competing mode

--- src/main/java/tictactoe/Config.java
+++ src/main/java/tictactoe/Config.java
@@ -1,5 +1,9 @@
 package tictactoe;
 
+import tictactoe.GameMechanics.Mode;
+
 public class Config {
     public static final boolean PRINT_BOARDS = false;
+    public static final int NUM_GAMES = 100;
+    public static final Mode MODE = Mode.TRAINING;
 }
---
+++ src/main/java/tictactoe/GameMechanics/Mode.java
@@ -0,0 +1,5 @@
+package tictactoe.GameMechanics;
+
+public enum Mode {
+    TRAINING, COMPETING
+}
