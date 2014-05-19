d1ad43b97f78862d9932453f36500a2c2c0a3128
Chris Cowell-Shah <chris.cowell-shah@jivesoftware.com>
2014-05-18 23:24:30 -0700

    add config options

--- src/main/java/tictactoe/Config.java
+++ src/main/java/tictactoe/Config.java
@@ -1,9 +1,8 @@
 package tictactoe;
 
-import tictactoe.GameMechanics.Mode;
-
 public class Config {
-    public static final boolean PRINT_BOARDS = false;
-    public static final int NUM_GAMES = 100;
-    public static final Mode MODE = Mode.TRAINING;
+    public static final boolean PRINT_GAMES = false;
+    public static final boolean PRINT_BIG_DATA = true;
+    public static final int NUM_TRAINING_GAMES = 10;
+    public static final int NUM_COMPETITION_GAMES = 3;
 }
