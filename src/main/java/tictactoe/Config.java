ad117e887609c789e694ff89a7ee022c0975dd77
Chris Cowell-Shah <cwcowellshah@gmail.com>
2014-05-19 21:47:50 -0700

    clean config file formatting

--- src/main/java/tictactoe/Config.java
+++ src/main/java/tictactoe/Config.java
@@ -1,8 +1,19 @@
 package tictactoe;
 
+/**
+ * All of the configurable dials for the program.
+ */
 public class Config {
+
+    /* the more games it runs during the training phase, the better its data analysis becomes */
+    public static final int NUM_TRAINING_GAMES = 10000000;
+
+    /* the more games it runs during the competition phase, the more accurate the data analysis advantage will be */
+    public static final int NUM_COMPETITION_GAMES = 100;
+
+    /* whether to print an ASCII representation of each move in both the training and competition phases */
     public static final boolean PRINT_GAMES = false;
-    public static final boolean PRINT_BIG_DATA = false;
-    public static final int NUM_TRAINING_GAMES = 1000;
-    public static final int NUM_COMPETITION_GAMES = 1000;
+
+    /* whether to print the contents of what it learned after the training phase completes, in mostly human-readable form */
+    public static final boolean PRINT_BIG_DATA = true;
 }
